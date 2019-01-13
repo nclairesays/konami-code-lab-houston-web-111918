2bdf39bfefa8c3923ac17b582a14b24fa6adec12
claire <github email address>
2019-01-13 13:58:43 -0600

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,39 @@ const codes = [
   "a"
 ];
 
+// Keep track of index outside of the event handler.
+let i = 0;
+
 function init() {
   // your code here
+  document.body.addEventListener('keydown', function(e){
+   
+    if (e.key === codes[i]){ 
+      i++
+      if (i == codes.length){
+        alert('Hooray!')
+      }
+    }
+
+  })
 }
+
+
+init()
+
+
+// var keyStroke = document.body.addEventListener('keydown', (event) => {
+
+//   if (event.key == codes[i]){
+//   i++;
+
+//   if (i == codes.length){
+//     alert('Hooray!')
+//   }
+// } else {
+// i = 0;
+// }
+// });
+
+
+ 
